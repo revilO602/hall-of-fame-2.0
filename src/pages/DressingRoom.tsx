de06b0a6fa2f5d5691409550782730f4347d0eca
Oliver Leontiev <leontiev.oliver@gmail.com>
2025-02-11 12:12:59 +0100

    feat[TBD]: zoom restrictions, light toggle

--- src/pages/DressingRoom.tsx
+++ src/pages/DressingRoom.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
-import DressingRoomImg from "/src/assets/dressRoomV4.png";
+import DressingRoomImgV4 from "/src/assets/dressRoomV4.png";
+import DressingRoomImgV3 from "/src/assets/dressRoomV3.png";
 
 function DressingRoom() {
+  const [currentImage, setCurrentImage] = useState(DressingRoomImgV4);
+
+  const toggleImage = () => {
+    setCurrentImage((prevImage) =>
+      prevImage === DressingRoomImgV4 ? DressingRoomImgV3 : DressingRoomImgV4
+    );
+  };
+
   return (
     <>
       <div className="flex h-screen w-screen justify-center items-center">
         <ReactPhotoSphereViewer
-          src={DressingRoomImg}
+          src={currentImage}
           height={"100vh"}
           width={"100%"}
+          minFov={40}
+          maxFov={60}
         ></ReactPhotoSphereViewer>
+        <button
+          onClick={toggleImage}
+          className="absolute top-4 right-4 bg-blue-500 text-black p-2 rounded"
+        >
+          Toggle Lightning
+        </button>
       </div>
     </>
   );
