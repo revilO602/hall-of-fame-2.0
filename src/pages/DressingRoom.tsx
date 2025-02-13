c3786d4d89263bfcadc2bc53489a9a3237b1534e
Oliver Leontiev <leontiev.oliver@gmail.com>
2025-02-13 14:27:38 +0100

    feat - dressRoomV5

--- src/pages/DressingRoom.tsx
+++ src/pages/DressingRoom.tsx
@@ -1,33 +1,32 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
-import DressingRoomImgV4 from "/src/assets/dressRoomV4.png";
-import DressingRoomImgV3 from "/src/assets/dressRoomV3.png";
+import DressingRoomImg from "/src/assets/dressRoomV5.png";
 
 function DressingRoom() {
-  const [currentImage, setCurrentImage] = useState(DressingRoomImgV4);
+  // const [currentImage, setCurrentImage] = useState(DressingRoomImgV4);
 
-  const toggleImage = () => {
-    setCurrentImage((prevImage) =>
-      prevImage === DressingRoomImgV4 ? DressingRoomImgV3 : DressingRoomImgV4
-    );
-  };
+  // const toggleImage = () => {
+  //   setCurrentImage((prevImage) =>
+  //     prevImage === DressingRoomImgV4 ? DressingRoomImgV3 : DressingRoomImgV4
+  //   );
+  // };
 
   return (
     <>
       <div className="flex h-screen w-screen justify-center items-center">
         <ReactPhotoSphereViewer
-          src={currentImage}
+          src={DressingRoomImg}
           height={"100vh"}
           width={"100%"}
           minFov={40}
           maxFov={60}
         ></ReactPhotoSphereViewer>
-        <button
+        {/* <button
           onClick={toggleImage}
           className="absolute top-4 right-4 bg-blue-500 text-black p-2 rounded"
         >
           Toggle Lightning
-        </button>
+        </button> */}
       </div>
     </>
   );
